8e50a5a12898c42202a5a5d9463bc6a353d9544a
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-04-08 13:49:45 +0530

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
     // JSX
     return (
       <div className="app">
-        <h2>Welcome to my app!</h2>
+        <h2>BLOG POSTS!</h2>
         <form onSubmit={this.submit}>
           <div className="form-input">
             <input
